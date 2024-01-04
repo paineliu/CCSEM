4862ce249870303f1a283fa0c25ca27c25467bf9
liutingchao <liutingchao@hotmail.com>
2024-01-04 20:05:24 +0800

    modify default cmd param

--- common/cmd_parser.py
+++ common/cmd_parser.py
@@ -3,5 +3,7 @@ import argparse
 
 def parse_cmd_arg():
     parser = argparse.ArgumentParser(description='Parser to read your config file')
-    parser.add_argument('--config', type=str, help='path to config file')
+    # parser.add_argument('--config', type=str, default='./config/instance_segmentation/mask_rcnn_R_50_FPN_3x_handwritten.yaml', help='path to config file')
+    parser.add_argument('--config', type=str, default='./config/instance_segmentation/mask_rcnn_R_50_FPN_3x_kaiti.yaml', help='path to config file')
+    
     return parser.parse_args()
